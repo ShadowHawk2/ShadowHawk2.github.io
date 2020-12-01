9294f0cc3638c978865a0932a2d8b141219ed081
Adrian Johnsen <ajohnsen1234@gmail.com>
2020-12-01 09:15:43 +0100

    Initial commit

---
+++ JavaScript/Adrian-1.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+var a=5
+var b=10
+console.log(a+b)
+console.log(b-a)
+console.log(b/a)
+console.log(a*b)
+console.log(b%2)
+
+if(a==b){
+    console.log("Tala er like!")
+    }
+else if(a<b){
+    console.log("Talet er mindre!")
+}
+else if(a>b){
+    console.log("Talet er større!")
+}
+
+var i = 0;
+while(i < 21) {
+    console.log(i);
+    i++;
+}
